fa35a830435a5140ef47f7659b7de6827101b5c1
anshikachaturavedibtechcs22-bit <anshika.chaturavedibtechcs22@smslucknow.ac.in>
2025-09-22 01:45:54 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,14 @@
 
+# db.py
+import psycopg2
+import streamlit as st
+
+def get_connection():
+    conn = psycopg2.connect(
+        host=st.secrets["postgres"]["host"],
+        database=st.secrets["postgres"]["database"],
+        user=st.secrets["postgres"]["user"],
+        password=st.secrets["postgres"]["password"],
+        port=st.secrets["postgres"]["port"]
+    )
+    return conn
