7c3ea933f7b25c7ad60f830c780a45fe4212e6d0
anshikachaturavedibtechcs22-bit <anshika.chaturavedibtechcs22@smslucknow.ac.in>
2025-09-21 18:14:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,24 @@ from geopy.distance import geodesic
 import folium
 import streamlit.components.v1 as components
 import requests
+import os
+import gdown
+from keras.models import load_model
+
+# Local file name for your model
+MODEL_FILE = "Effi_WRM.keras"
+
+# Google Drive direct download URL
+GOOGLE_DRIVE_URL = "https://drive.google.com/uc?export=download&id=1XaSPF0H031LEauh8tvw67yFRSIDJWItg"
+
+# Download model if it doesn't exist
+if not os.path.exists(MODEL_FILE):
+    print("Downloading model from Google Drive...")
+    gdown.download(GOOGLE_DRIVE_URL, MODEL_FILE, quiet=False)
+
+# Load the model
+model = load_model(MODEL_FILE, compile=False)
+print("Model loaded successfully!")
 
 # ==========================
 # Page Config 
@@ -891,4 +909,5 @@ else:
     if page == "Home": render_home_page()
     elif page == "Waste Classifier": render_classifier_page()
     elif page == "Waste Types": render_waste_types_page()
+
     elif page == "Do's and Don'ts": render_dos_donts_page()
